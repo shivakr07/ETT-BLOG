e4a3617e46a987d6ca97f04055a5028b2d29c562
shivakr07 <kumarshiva0707@gmail.com>
2024-02-15 12:29:38 +0530

    header section done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <h1 className='text-red-500'>App</h1>
+
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
