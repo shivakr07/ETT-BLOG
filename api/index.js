38b1e109428f1b929766fc4c099df0e607452ce6
shivakr07 <kumarshiva0707@gmail.com>
2024-02-15 18:01:32 +0530

    signup api completed

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
 const PORT = 3000;
 const app = express()
+app.use(express.json())
 
 mongoose
 .connect(process.env.MONGO_URI)
@@ -19,4 +21,5 @@ mongoose
     console.log("some error while connecting to the db", error);
 })
 
-app.use('/api/user/', userRoutes)
+app.use('/api/user/', userRoutes);
+app.use('/api/auth', authRoutes);
